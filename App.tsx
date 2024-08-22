7de0c5995fab3e013b2e60fbf0600b84245f6d22
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-08-22 15:11:02 -0400

    initiated the project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { BalsamiqSans_400Regular,BalsamiqSans_700Bold, useFonts } from '@expo-google-fonts/balsamiq-sans'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_700Bold
+  })
+
+  console.log(fontsLoaded, 'linha12 app ')
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
