7dd824dda9a2a6eaeb3f172efc30ac7b5a0f71c4
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-08-22 17:00:48 -0400

    feat: add AllExpenses, ManageExpense, RecentExpenses Screens

---
+++ src/screens/ManageExpense/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+
+export const ManageExpense = ()=>{
+    return (<View>
+        <Text>Manage Expenses</Text>
+    </View>)
+};
