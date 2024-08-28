7e0624d36a9f02bdb90fc77edc39a528d6843e04
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-08-28 09:13:29 -0400

    refactor: add  more data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,12 +2,12 @@
 export const data = [
     {
         id: 'Exp-' + Math.floor(Math.random() * 1000000),
-        description: 'A pair of shoes',
-        amount: 59.00,
-        date: new Date('2024-08-23'),
-        category: 'Apparel'
-
+        description: 'Bread',
+        amount: 7.50,
+        date: new Date('2024-08-26'),
+        category: 'Groceries'
     },
+    
     {
         id: 'Exp-' + Math.floor(Math.random() * 1000000),
         description: 'A pair of sandals',
@@ -28,7 +28,7 @@ export const data = [
         description: 'Snow boots',
         amount: 59.00,
         date: new Date('2024-08-20'),
-        category: 'Apparel'
+        category: 'Sports'
     },
     
     {
@@ -47,7 +47,21 @@ export const data = [
         date: new Date('2024-08-26'),
         category: 'Groceries'
     },
-    
+    {
+        id: 'Exp-' + Math.floor(Math.random() * 1000000),
+        description: 'Bicycle',
+        amount: 159.00,
+        date: new Date('2024-08-20'),
+        category: 'Sports'
+    },
+    {
+        id: 'Exp-' + Math.floor(Math.random() * 1000000),
+        description: 'A pair of shoes',
+        amount: 59.00,
+        date: new Date('2024-08-23'),
+        category: 'Apparel'
+
+    },
     
 ]
 
