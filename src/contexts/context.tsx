f4ff6496c536bd866cb0ce5cbe773c84735b533b
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-09-16 21:35:12 -0400

    refactor: remove Omit from addExpense

--- src/contexts/context.tsx
+++ src/contexts/context.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useReducer } from "react";
+import { createContext, ReactNode, useReducer , useEffect } from "react";
+
 
 export type ExpIdType = {
     id: string
@@ -20,109 +21,112 @@ export type expenseType = {
 export const ExpensesCtx = createContext({
   expenses: [],
   addExpense: (
-    expense: Omit<expenseType,'id'>) => {},
+    expense: expenseType) => {},
   deleteExpense: (id: ExpIdType) => {},
   updateExpense: (
     id: ExpIdType, expense: Omit<expenseType, 'id'>
   ) => {},
+  setExpenses: (expenses: expenseType[])=>{}
 });
 
-const initialState = [
-    {
-        id: 'Exp-' + Math.floor(Math.random() * 1000000),
-        description: 'Shin guards',
-        amount: 22.38,
-        date: new Date('2024-08-29'),
-        category: 'Sports'
-    },
-    {
-        id: 'Exp-' + Math.floor(Math.random() * 1000000),
-        description: 'Rolling blades',
-        amount: 22.38,
-        date: new Date('2024-08-28'),
-        category: 'Sports'
-    },
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Soccer Ball',
-      amount: 42.50,
-      date: new Date(),
-      category: 'Sports'
-  },
+// const initialState = [
+//     {
+//         id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//         description: 'Shin guards',
+//         amount: 22.38,
+//         date: new Date('2024-08-29'),
+//         category: 'Sports'
+//     },
+//     {
+//         id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//         description: 'Rolling blades',
+//         amount: 22.38,
+//         date: new Date('2024-08-28'),
+//         category: 'Sports'
+//     },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Soccer Ball',
+//       amount: 42.50,
+//       date: new Date(),
+//       category: 'Sports'
+//   },
   
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Bread',
-      amount: 7.50,
-      date: new Date('2024-08-26'),
-      category: 'Groceries'
-  },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Bread',
+//       amount: 7.50,
+//       date: new Date('2024-08-26'),
+//       category: 'Groceries'
+//   },
   
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'A pair of sandals',
-      amount: 59.00,
-      date: new Date('2024-08-24'),
-      category: 'Apparel'
-  },
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'T-shirt',
-      amount: 59.45,
-      date: new Date('2024-08-21'),
-      category: 'Apparel'
-  },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'A pair of sandals',
+//       amount: 59.00,
+//       date: new Date('2024-08-24'),
+//       category: 'Apparel'
+//   },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'T-shirt',
+//       amount: 59.45,
+//       date: new Date('2024-08-21'),
+//       category: 'Apparel'
+//   },
   
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Snow boots',
-      amount: 59.00,
-      date: new Date('2024-08-20'),
-      category: 'Sports'
-  },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Snow boots',
+//       amount: 59.00,
+//       date: new Date('2024-08-20'),
+//       category: 'Sports'
+//   },
   
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Bicycle',
-      amount: 59.00,
-      date: new Date('2024-08-25'),
-      category: 'Sports'
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Bicycle',
+//       amount: 59.00,
+//       date: new Date('2024-08-25'),
+//       category: 'Sports'
 
-  },
+//   },
   
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Vegetables',
-      amount: 59.00,
-      date: new Date('2024-08-26'),
-      category: 'Groceries'
-  },
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'Bicycle',
-      amount: 159.00,
-      date: new Date('2024-08-20'),
-      category: 'Sports'
-  },
-  {
-      id: 'Exp-' + Math.floor(Math.random() * 1000000),
-      description: 'A pair of shoes',
-      amount: 59.00,
-      date: new Date('2024-08-23'),
-      category: 'Apparel'
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Vegetables',
+//       amount: 59.00,
+//       date: new Date('2024-08-26'),
+//       category: 'Groceries'
+//   },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'Bicycle',
+//       amount: 159.00,
+//       date: new Date('2024-08-20'),
+//       category: 'Sports'
+//   },
+//   {
+//       id: 'Exp-' + Math.floor(Math.random() * 1000000),
+//       description: 'A pair of shoes',
+//       amount: 59.00,
+//       date: new Date('2024-08-23'),
+//       category: 'Apparel'
 
-  },
+//   },
   
-]
-
+// ]
+const initialState =  [{}];
 
 
 const expenseReducer = (state: any, action: any)=>{
     switch(action.type){
+        case 'SET':
+         const invertDateOrder = action.payload.reverse();
+         return invertDateOrder;  
         case 'ADD': 
         const currentExpenses = [...state];
-        const expId = 'Exp-' + Math.floor(Math.random() * 1000000)
-        return [{ ...action.payload , id: expId }, ...currentExpenses ]
+        return [action.payload  , ...currentExpenses];
         case 'UPDATE':
             const expIndex = state.findIndex((expense:expenseType) => expense.id === action.payload.id.id);
             const expense = state[expIndex]
@@ -138,6 +142,7 @@ const expenseReducer = (state: any, action: any)=>{
     }
 }
 export const ExpensesCtxProvider = ({ children }: ExpensesCtxProviderType) => {
+
     const [expensesState, dispatch] = useReducer(expenseReducer, initialState);
 
     const addExpense =(expenseData: Omit<expenseType, 'id'>)=>{
@@ -149,18 +154,22 @@ export const ExpensesCtxProvider = ({ children }: ExpensesCtxProviderType) => {
         dispatch( {type: 'UPDATE', payload: { id: id, data: expenseData }})
     };
 
-
     const deleteExpense =(id: ExpIdType)=>{
- 
         dispatch( {type: 'DELETE', payload: id})
     };
 
+    const setExpenses = (expenses:expenseType[] )=>{
+        dispatch({type: 'SET', payload: expenses})
+    }
+
+  
 
     const value ={
         expenses: expensesState,
         addExpense: addExpense,
         updateExpense: updateExpense,
-        deleteExpense: deleteExpense
+        deleteExpense: deleteExpense,
+        setExpenses: setExpenses
 
     }
 
