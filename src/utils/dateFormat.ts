35b8c16c1cd71b26dc0c693655a0a388ea224e20
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-09-26 08:32:34 -0400

    refactor: modified return of dateFormat function

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,4 +1,11 @@
 export const dateFormat = (date: Date)=>{
-    return date.toISOString().slice(0,10);
-    return `${date.getMonth()+1}-${date.getUTCDate()}-${date.getFullYear()}`
+    // return date.toISOString().slice(0,10);
+    let dateDoubleDigits = '';
+    let monthDoubleDigits = '';
+
+    monthDoubleDigits = date.getMonth() + 1 < 10 ? `0${date.getMonth()+1}`: `${date.getMonth() +1 }`
+    dateDoubleDigits = date.getDate() < 10 ? `0${date.getDate()}`: `${date.getDate()}`
+    
+
+    return `${monthDoubleDigits}-${dateDoubleDigits}-${date.getFullYear()}`;
 }
