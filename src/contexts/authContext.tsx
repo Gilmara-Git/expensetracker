1997fffeae06f47d63b95a5c6a1915ada1afe9ff
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-10-03 21:14:26 -0400

    refactor: use token storage and user storage

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,106 +1,135 @@
-import { createContext,  ReactNode, useState } from "react";
-import axios from 'axios';
-import { API_KEY } from '@env';
+import { createContext, ReactNode, useState, useEffect } from "react";
+import axios from "axios";
+import { API_KEY } from "@env";
 import { userBackend } from "@services/authenticateUser";
+import {
+  setUserStorage,
+  getUserStorage,
+  removeUserStorage,
+} from "@storage/userStorage";
+import {
+  setTokenStorage,
+  removeTokenStorage,
+} from "@storage/tokenStorage";
 
 const baseUrl = userBackend.getUri();
 
-
-type UserType = {
-    email: string,
-    idToken: string,
-    refreshToken : string,
-    expiresIn: string,
-    uid: string,
-    registered: boolean;
-}
+export type UserType = {
+  email: string;
+  idToken: string;
+  refreshToken: string;
+  expiresIn: string;
+  uid: string;
+  registered: boolean;
+};
 
 type AuthContextDataProps = {
-    user: UserType,
-    signIn: (email: string, password: string)=>Promise<void>,
-    signUp: (email: string, password: string)=>Promise<void>
-    signOut:()=>Promise<void>
-}
+  user: UserType;
+  signIn: (email: string, password: string) => Promise<void>;
+  signUp: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+};
 
 type AuthContextProvider = {
-    children: ReactNode;
-}
-export const AuthContext = createContext<AuthContextDataProps>({}as AuthContextDataProps);
+  children: ReactNode;
+};
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
 
+export const AuthContextProvider = ({ children }: AuthContextProvider) => {
+  const [user, setUser] = useState<UserType>({} as UserType);
 
-export const AuthContextProvider = ({children}: AuthContextProvider)=>{
-    const [ user, setUser ] = useState<UserType>({} as UserType );
+  const signUp = async (email: string, password: string) => {
+    try {
+      const response = await axios.post(
+        `${baseUrl}signUp?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      // axios.defaults.headers.common["Authorization"] = response.data.idToken
+      // axios.interceptors.response.use
 
+      const userDetails: UserType = {
+        email: response.data.email,
+        idToken: response.data.idToken,
+        refreshToken: response.data.refreshToken,
+        expiresIn: response.data.expiresIn,
+        uid: response.data.localId,
+        registered: true,
+      };
 
-    const signUp = async (email: string, password: string)=> {
+      await setUserStorage(userDetails);
+      await setTokenStorage(userDetails.idToken, userDetails.refreshToken);
+      setUser(userDetails);
+    } catch (error) {
+      throw error;
+    }
+  };
 
-        try{
-            const response = await axios.post(`${baseUrl}signUp?key=${API_KEY}`, 
-                {
-                    email: email,
-                    password: password,
-                    returnSecureToken: true
-                },
-                { headers: {'Content-Type': 'application/json' }
-            } 
-        )
-        
-        const userDetails =  {
-            email: response.data.email,
-            idToken: response.data.idToken,
-            refreshToken : response.data.refreshToken,
-            expiresIn: response.data.expiresIn,
-            uid: response.data.localId,
-            registered: true   
+  const signIn = async (email: string, password: string) => {
+    try {
+      const response = await axios.post(
+        `${baseUrl}signInWithPassword?key=${API_KEY}`,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
         }
-    
-        setUser(userDetails)
+      );
 
-        }catch(error){
-            throw error;
-        }
-  
+      const userDetails: UserType = {
+        email: response.data.email,
+        idToken: response.data.idToken,
+        refreshToken: response.data.refreshToken,
+        expiresIn: response.data.expiresIn,
+        uid: response.data.localId,
+        registered: response.data.registered,
+      };
+
+      await setUserStorage(userDetails);
+      await setTokenStorage(userDetails.idToken, userDetails.refreshToken);
+      setUser(userDetails);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      setUser({} as UserType);
+      removeUserStorage();
+      removeTokenStorage();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    //sending the signOut function to the api/userBackend so we can use it on the 'intercepting the backend response'
+    const subscribe = userBackend.registerInterceptTokenManager(signOut);
+
+    // cleanup function after application loads
+    return () => {
+      subscribe();
     };
+  }, [signOut]);
 
-    const signIn = async (email: string, password: string)=>{
+  useEffect(() => {
+    const getUserDetails = async () => {
+      const userInStorage = await getUserStorage();
+      setUser(userInStorage);
+    };
+    getUserDetails();
+  }, []);
 
-        try{
-            const response =  await axios.post(`${baseUrl}signInWithPassword?key=${API_KEY}`, {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            })
-        
-            const userDetails =  {
-                email: response.data.email,
-                idToken: response.data.idToken,
-                refreshToken : response.data.refreshToken,
-                expiresIn: response.data.expiresIn,
-                uid: response.data.localId,
-                registered: response.data.registered
-            }
-           
-            setUser(userDetails)
-
-
-        }catch(error){ 
-            throw error;
-        }
-    }
-
-    const signOut = async()=>{
-        try{
-            setUser({}as UserType)
-        }catch(error){
-            throw error;
-        }
-    }
-
-
-
-    return (
-        <AuthContext.Provider value={{user, signIn, signUp, signOut}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ user, signIn, signUp, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,31 +1,29 @@
-import { useState } from 'react';
-import { View} from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
 import { styles } from "./styles";
 import { LinearGradient } from "expo-linear-gradient";
-import Toast from 'react-native-toast-message';
 
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavProps } from "@routes/auth.routes";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { Loading } from '@components/Loading';
-
+import { Loading } from "@components/Loading";
 import { SignInSingUpLink } from "@components/SignInSignUpLink";
-// import { userSignIn } from "@services/authenticateUser";
-import themes from '../../theme/themes';
 
-import { z} from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import themes from "../../theme/themes";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
-import { useUserContext } from '@hooks/useUserContext';
-
+import { useUserContext } from "@hooks/useUserContext";
+import Toast from "react-native-toast-message";
 
 const SignInSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6).max(8)
-})
+  password: z.string().min(6).max(8),
+});
 
-type FormData = z.infer<typeof SignInSchema>
+type FormData = z.infer<typeof SignInSchema>;
 
 export const SignIn = () => {
   const {
@@ -33,127 +31,155 @@ export const SignIn = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>({
-    resolver: zodResolver(SignInSchema)
+    resolver: zodResolver(SignInSchema),
   });
 
-  const [ isAuthenticating, setIsAuthenticating] = useState(false);
-  const [ showPassword, setShowPassword ] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigation = useNavigation<AuthNavProps>();
-  const { signIn }  = useUserContext();
-  
-  const showToast = ()=>{
+  const { signIn } = useUserContext();
+
+  const showToast = (errorMessage: string) => {
     Toast.show({
-      type: 'success',
-      text1: 'Your credentials are incorrect.',
-      text2: 'Verify your email and password',
-      text1Style: {color: themes.colors.purple_1, fontSize: 16, fontFamily: themes.fonts.balsamiq_700},
-      text2Style: {color: themes.colors.warn, fontSize:14, fontFamily: themes.fonts.balsamiq_400},
-      position: 'top',
+      type: "error",
+      text1: errorMessage ? JSON.stringify(errorMessage) : "Try again later.",
+      text2: '"Verify your email and password"',
+      text1Style: {
+        color: themes.colors.purple_1,
+        fontSize: 14,
+        fontFamily: themes.fonts.balsamiq_700,
+      },
+      text2Style: {
+        color: themes.colors.warn,
+        fontSize: 14,
+        fontFamily: themes.fonts.balsamiq_400,
+      },
+      position: "top",
       autoHide: true,
-      visibilityTime:4000, 
-      topOffset:140
-     })
+      visibilityTime: 4000,
+      topOffset: 140,
+    });
   };
 
-
-  const handleToggleReviewPassword = ()=>{
-    console.log('I was clicked')
-    setShowPassword((preState)=> !preState);
-
-  }
+  const handleToggleReviewPassword = () => {
+    setShowPassword((preState) => !preState);
+  };
 
   const handleConfirm = async (fields: FormData) => {
-    console.log(fields)
-    try{
+    try {
       setIsAuthenticating(true);
       await signIn(fields.email, fields.password);
-     
-    }catch(error:any){
-      if(error.response){
-        if(error.response.status === 400 && error.response.data.error.message === 'INVALID_LOGIN_CREDENTIALS'){
-       
-          showToast()
+    } catch (error: any) {
+      if (error.response) {
+        if (
+          error.response.status === 400 &&
+          error.response.data.error.message === "INVALID_LOGIN_CREDENTIALS"
+        ) {
+          showToast(error.response.data.error.message);
         }
-        console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.response.data.error.message);
-        console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.response.status);
-        console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.response.headers);
+        if (error.response.data.error.message) {
+          console.log(
+            "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Error message",
+            error.response.data.error.message
+          );
+        }
 
-      }else if(error.request){
-        console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.request)
-
-      }else{
-        console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.message);
+        if (error.response.status) {
+          console.log(
+            "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Status",
+            error.response.status
+          );
+        }
+        console.log(
+          "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Headers",
+          error.response.headers
+        );
+      } else if (error.request) {
+        console.log(
+          "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Error request",
+          error.request
+        );
+      } else {
+        console.log(
+          "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Error message",
+          error.message
+        );
       }
-      console.log('CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN',error.config);
-     
-    }finally{
-      setIsAuthenticating(false)
+      console.log(
+        "CAUGHT_API_REQUEST_ERROR_SIGNIN_SCREEN - Error config",
+        error.config
+      );
+    } finally {
+      setIsAuthenticating(false);
     }
   };
 
-  const directToSignUn =()=>{
-    navigation.navigate('signUp')
+  const directToSignUn = () => {
+    navigation.navigate("signUp");
   };
 
- 
   return (
     <LinearGradient colors={["#f2edf3", "#c199ea"]} style={styles.background}>
-      { isAuthenticating ? <Loading message='Login in process..'/>  :
-      <View style={styles.container}>
-        <View>
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-              label="Email"
-              multiline={false}
-              errorMessage={errors.email?.message}
-              placeholder="john@example.com"
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              autoCapitalize='none'
-              />
-            )}
-            name="email"
+      {isAuthenticating ? (
+        <Loading message="Login in process.." />
+      ) : (
+        <View style={styles.container}>
+          <View>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Email"
+                  multiline={false}
+                  errorMessage={errors.email?.message}
+                  placeholder="john@example.com"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  autoCapitalize="none"
+                />
+              )}
+              name="email"
             />
-        </View>
+          </View>
 
-        <View>
-          <Controller
-            control={control}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input
-              label="Password"
-              multiline={false}
-              errorMessage={errors.password?.message}
-              placeholder="password"
-              value={value}
-              onChangeText={onChange}
-              onBlur={onBlur}
-              returnKeyType='send'
-              onSubmitEditing={handleSubmit(handleConfirm)}
-              secureTextEntry={!showPassword}
-              passwordField={true}
-              shouldReviewPassword={handleToggleReviewPassword}
-              showPassword={showPassword}
-              autoCorrect={false}
-              autoCapitalize='none'
-            
-              />
-            )}
-            name="password"
+          <View>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  label="Password"
+                  multiline={false}
+                  errorMessage={errors.password?.message}
+                  placeholder="password"
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  returnKeyType="send"
+                  onSubmitEditing={handleSubmit(handleConfirm)}
+                  secureTextEntry={!showPassword}
+                  passwordField={true}
+                  shouldReviewPassword={handleToggleReviewPassword}
+                  showPassword={showPassword}
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                />
+              )}
+              name="password"
             />
+          </View>
+          <View style={styles.button}>
+            <Button title="Login" onPress={handleSubmit(handleConfirm)} />
+          </View>
+          <View style={styles.button}>
+            <SignInSingUpLink
+              question="New user?"
+              direction="Create account"
+              onClick={directToSignUn}
+            />
+          </View>
         </View>
-        <View style={styles.button}>
-          <Button title="Login" onPress={handleSubmit(handleConfirm)} />
-        </View>
-        <View style={styles.button}>
-         <SignInSingUpLink question='New user?' direction='Create account' onClick={directToSignUn}/>
-        </View>
-
-      </View>
-      }
+      )}
     </LinearGradient>
   );
 };
