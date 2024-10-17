d85bd12f15c68398509ccadbd26d36295c071e15
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-10-17 18:08:20 -0400

    feat: set permission to camera and configured camera usage utilizing ImagePicker

--- src/screens/PhotoMap/index.tsx
+++ src/screens/PhotoMap/index.tsx
@@ -1,14 +1,70 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View , Alert, Image, Text} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
-import { IconButton } from '@components/IconButton';
 import themes from '../../theme/themes';
+import { IconButton } from '@components/IconButton';
+import  { Button } from '@components/Button';
+import *  as ImagePicker from 'expo-image-picker';
 
 
+
+
+type ImageType = {
+    uri: string,
+    fileName: string | null | undefined
+}
 
 export const PhotoMap = ()=>{
-    const handleUploadReceipt = ()=>{
-        console.log('Upload Photo')
+
+    const [receiptImage, setReceiptImage] = useState<ImageType>({} as ImageType);
+    const [ cameraPermission, requestPermission ]=  ImagePicker.useCameraPermissions();
+
+    const requestPermissionIOSNeedsCameraPermissions = async()=>{
+        // check if there is no permission and if it is not granted
+      
+           if(cameraPermission?.status === ImagePicker.PermissionStatus.UNDETERMINED){
+               const response =  await requestPermission();
+               // this returns true of false
+               return response.granted
+
+           }
+
+           if(cameraPermission?.status === ImagePicker.PermissionStatus.DENIED){
+            Alert.alert('Permission to Camera was denied','You need to allow camera usage for this app to take photos')
+            return false;
+
+        }
+   
+            return true;
+
+    }
+    
+    
+    const handleUploadReceipt = async()=>{
+
+        // ios needs permission to use the camera. Even though android does not, we will return if there is not permission
+        const isPermissionGranted = await requestPermissionIOSNeedsCameraPermissions();
+
+        if(!isPermissionGranted){
+            return;
+        }
+
+        const receipt = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [6,4],
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+            cameraType: ImagePicker.CameraType.back
+
+
+        })
+        if(receipt.assets){
+            setReceiptImage({uri: receipt.assets[0].uri, fileName: receipt.assets[0].fileName})
+            console.log(receipt.assets[0].fileName, receipt.assets[0].uri)
+
+        }
+     
     };
     const handleLocation =()=>{
         console.log('Location')
@@ -22,18 +78,52 @@ export const PhotoMap = ()=>{
                 <View style={styles.wrappingContainer}>
                     <View style={styles.innerWrapper}>
                 
-                <IconButton iconName="receipt-sharp" size={40} color={themes.colors.purple_2} uploadReceipt={handleUploadReceipt}/>
                       
+                {
+                    receiptImage.uri ? 
+                        <View style={styles.imagePreview}>
+                            <Image style={styles.imageTaken} source={{uri: receiptImage.uri}} /> 
+                            <Button style={styles.trashButton}
+                                onPress={()=>setReceiptImage({} as ImageType)}
+                                icon={
+                                    <IconButton
+                                    disabled
+                                    iconName="trash-outline"
+                                    size={20}
+                                    color={themes.colors.warn}
+                                    />
+              }
+            />
 
+                        </View>
+
+                    :
+                    <View style={styles.photoIconContainer}>
+                        <IconButton 
+                            iconName="receipt-sharp" 
+                            size={35} 
+                            color={themes.colors.purple_2} 
+                            uploadReceipt={handleUploadReceipt}/>
+
+                            <Text style={styles.iconTitle}>Upload Receipt</Text>
+                    </View>
+
+
+                }
                     </View>
 
                 </View>
             </View>
                 <View style={styles.map}>
                 <View style={styles.wrappingContainer}>
-                    <View style={styles.innerWrapper}>
-                    <IconButton iconName="google-maps" size={40} color={themes.colors.purple_2} loadMap={handleLocation}/>
-
+                        <View style={styles.innerWrapper}>
+                            <IconButton 
+                                iconName="google-maps" 
+                                size={35} 
+                                color={themes.colors.purple_2} 
+                                loadMap={handleLocation}/>
+                                
+                            <Text style={styles.iconTitle}>Load Map</Text>
                     </View>
                 </View>
                 </View>
--- src/screens/PhotoMap/styles.ts
+++ src/screens/PhotoMap/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import themes from "../../theme/themes";
+import { Platform } from 'react-native';
 
 export const styles = StyleSheet.create({
   background: {
@@ -8,28 +9,68 @@ export const styles = StyleSheet.create({
     right: 0,
     top: 0,
     bottom: 0,
+
   },
+  // overflow and borderRadius applied on wrappingContainer, so borderRadius and Shadow work on both platform
   photo: {
     flex: 1,
     margin: 30,
-    borderRadius: 8,
-    overflow: "hidden",
-  
+    elevation: 4,
+    shadowColor: themes.colors.jetBlack,
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: .3,
+    shadowRadius: 2,
+   
   },
+  // overflow and borderRadius applied on wrappingContainer, so borderRadius and Shadow work on both platform
     map: {
-    flex: 2,
+    flex: 1,
     margin: 30,
     marginTop: -20,
-    borderRadius: 8,
-    overflow: "hidden",
+    elevation: 4,
+    shadowColor: themes.colors.jetBlack,
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: .3,
+    shadowRadius: 2,
  
   },
   wrappingContainer: {
-    backgroundColor: themes.colors.light_purple,
+    backgroundColor: themes.colors.light_white,
     flex: 1,
+    overflow: 'hidden',
+    borderRadius: 8,
+
   },
   innerWrapper:{
     justifyContent: "center", 
     alignItems: "center", 
-    flex:1}
+    flex:1,
+   
+  
+  }
+    , 
+    imagePreview:{
+      width: '100%',
+      height: '100%',
+
+
+    }, 
+    imageTaken:{
+      width: '100%',
+      height: '100%',
+      borderRadius: 8
+    },
+    trashButton: { 
+      position: 'absolute', 
+      bottom:10, 
+      right: 10 
+    },
+    photoIconContainer:{
+      alignItems: 'center',
+    },
+    iconTitle: {
+      fontFamily: themes.fonts.balsamiq_400, 
+      fontSize: 12 , 
+      color: themes.colors.purple_1}
+      
 });
