2b2d72fa805e22d4f3c2e009c4f3fae025eba241
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-10-24 20:13:49 -0400

    refactor: add marker to mapStatic url

--- src/utils/mapStaticPreview.ts
+++ src/utils/mapStaticPreview.ts
@@ -1,6 +1,7 @@
 import { GOOGLE_MAP_STATIC_API } from "@env";
 
 export const mapStaticPreviewURL = (lat:number, long: number)=>{
+
     let url = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${long}&zoom=12&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${long}&key=${GOOGLE_MAP_STATIC_API}`
 
     return url;
