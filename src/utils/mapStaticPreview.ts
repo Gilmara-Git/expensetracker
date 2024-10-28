0575b36a5e094a0371e6d32abbd125297c02d25c
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-10-28 09:34:04 -0400

    refactor: replaced Google API Key  with variable GOOGLE_MAP_STATIC_API

--- src/utils/mapStaticPreview.ts
+++ src/utils/mapStaticPreview.ts
@@ -8,4 +8,4 @@ export const mapStaticPreviewURL = (lat:number, long: number)=>{
 }
 
 // If you pass the city and state, it will return your city
-//`https://maps.googleapis.com/maps/api/staticmap?center=Elizabeth,NJ&zoom=12&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${long}&key=AIzaSyD9Ao4G3KxJZus2C_IMLOsgm_tGcJX_Ah8`
+//`https://maps.googleapis.com/maps/api/staticmap?center=Elizabeth,NJ&zoom=12&size=400x200&maptype=roadmap&markers=color:red%7Clabel:C%7C${lat},${long}&key=${GOOGLE_MAP_STATIC_API}`
